2625d1048e8e29b0f2cad506411f3660cbeb3fa0
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-09-10 13:40:22 +0300

    changed resources

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties",
+        "classpath:{env}.properties",
         "classpath:local.properties"
 })
 public interface WebConfig extends Config {
